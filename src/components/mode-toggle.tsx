5fd50a4ca4119aa6e8cb50a24ef76f3734d7e70a
mi-tec <migara1225@gmail.com>
2025-10-29 15:02:45 +0000

    made cv downloadeable

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,25 +1,29 @@
-import { Moon, Sun } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { useTheme } from "@/components/theme-provider"
-import { useState } from "react"
+import CV from "@/assets/Migara_Karunarathne_CV.pdf";
+import { useTheme } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react";
 
 export function ModeToggle() {
-    const { setTheme, theme } = useTheme()
-    const [currentTheme, setCurrentTheme] = useState(theme)
+  const { setTheme, theme } = useTheme();
+  const [currentTheme, setCurrentTheme] = useState(theme);
 
-    const toggleTheme = () => {
-        const nextTheme = currentTheme === "light" ? "dark" : "light"
-        setCurrentTheme(nextTheme)
-        setTheme(nextTheme)
-    }
+  const toggleTheme = () => {
+    const nextTheme = currentTheme === "light" ? "dark" : "light";
+    setCurrentTheme(nextTheme);
+    setTheme(nextTheme);
+  };
 
-    return (
-        <div className="flex flex-row-reverse">
-            <Button variant="outline" size="icon" onClick={toggleTheme}>
-                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                <span className="sr-only">Toggle theme</span>
-            </Button>
-        </div>
-    )
+  return (
+    <div className="flex flex-row-reverse space-x-2 space-x-reverse">
+      <Button variant="outline" size="icon" onClick={toggleTheme}>
+        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+      <a href={CV} download="Migara Karunarathne CV" target="_blank">
+        <Button variant="outline">Download CV</Button>
+      </a>
+    </div>
+  );
 }
